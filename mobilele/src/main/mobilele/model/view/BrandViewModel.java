d937091125a5c161316f2e4c27896d3ad7df90da
DesislavaGetova-Petrova <49076927+DesislavaGetova-Petrova@users.noreply.github.com>
2021-01-31 21:38:01 +0200

    Update BrandViewModel.java

--- mobilele/src/main/mobilele/model/view/BrandViewModel.java
+++ mobilele/src/main/mobilele/model/view/BrandViewModel.java
@@ -3,37 +3,40 @@ package softuni.mobilele.mobilele.model.view;
 import java.util.List;
 
 public class BrandViewModel {
-    private String name;
 
-    private List<ModelViewModel> models;
+  private String name;
 
-    public String getName() {
-        return name;
-    }
+  private List<ModelViewModel> models = new ArrayList<>();
 
-    public BrandViewModel setName(String name) {
-        this.name = name;
-        return this;
-    }
+  public List<ModelViewModel> getModels() {
+    return models;
+  }
 
-    public List<ModelViewModel> getModels() {
-        return models;
-    }
-    public BrandViewModel setModels(List<ModelViewModel> models) {
-        this.models = models;
-        return this;
-    }
-    public BrandViewModel addModel(ModelViewModel modelViewModel) {
-        this.models.add(modelViewModel);
-        return this;
-    }
+  public BrandViewModel addModel(ModelViewModel modelViewModel) {
+    this.models.add(modelViewModel);
+    return this;
+  }
 
+  public BrandViewModel setModels(
+      List<ModelViewModel> models) {
+    this.models = models;
+    return this;
+  }
 
-    @Override
-    public String toString() {
-        return "BrandViewModel{" +
-                "name='" + name + '\'' +
-                ", models=" + models +
-                '}';
-    }
+  public String getName() {
+    return name;
+  }
+
+  public BrandViewModel setName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return "BrandViewModel{" +
+        "name='" + name + '\'' +
+        ", models=" + models +
+        '}';
+  }
 }
